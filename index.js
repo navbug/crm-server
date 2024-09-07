d51b133b0247fabe0a1e6709428f0072cbf7a25e
Naveen Bugalia <naveenbugalia512@gmail.com>
2024-09-07 16:13:11 +0530

    updated

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ app.use(cors({
     return callback(null, true);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 app.use(express.json());
 app.use(
